1e6f1e67bdabdd959660eccd81b9dcb27daf726a
Elizabeth Akindele <elizase405@gmail.com>
2024-07-01 08:35:59 +0000

    feat: task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    A module with 1 function: index_range(3, 15) - (30, 45)
+"""
+
+def index_range(page, page_size):
+    """
+        Calculate the start index and end index of a particular page
+
+        Args:
+            page (int) page no we currently want
+            page_size (int) no of items on the page
+        Return:
+            A tuple of size two containing a start index and an end index of the page
+    """
+
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return (start_index, end_index)
