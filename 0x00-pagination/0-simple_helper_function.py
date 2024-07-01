be580d77d916f598c9f11ecfe0374d86f79645cc
Elizabeth Akindele <elizase405@gmail.com>
2024-07-01 08:37:19 +0000

    chore: fix pycodestyle errors

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
     A module with 1 function: index_range(3, 15) - (30, 45)
 """
 
+
 def index_range(page, page_size):
     """
         Calculate the start index and end index of a particular page
@@ -11,7 +12,8 @@ def index_range(page, page_size):
             page (int) page no we currently want
             page_size (int) no of items on the page
         Return:
-            A tuple of size two containing a start index and an end index of the page
+            A tuple of size two containing a
+            start index and an end index of the page
     """
 
     start_index = (page - 1) * page_size
