c1c90dce27353d189b57fc72798a77f070c3600e
Elizabeth <elizase@gmail.com>
2024-09-12 13:00:34 +0000

    feat: task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Setting up a basic Flask app"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+# render html template on / route
+@app.route('/')
+def hello_world():
+    """renders template"""
+    return render_template('/templates/0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
